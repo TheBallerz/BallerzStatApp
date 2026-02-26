b8fbb8734c23a033c6de0191943cf3e9ebf5f056
DoanCal <doantcalpoly@gmail.com>
2026-02-26 05:59:37 -0800

    React Router Implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,38 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+
+
+function Home() {
+  return <h2>Welcome to Ballerz</h2>;
+}
+
+function Players() {
+  return <h2>Players Page</h2>;
+}
+
+
+function Teams() {
+  return <h2>Teams Page</h2>;
+}
+
 
 function App() {
   return (
     <div className="app">
       <h1>Ballerz Stat App</h1>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/players">Players</Link>
+        <Link to="/teams">Teams</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/players" element={<Players />} />
+        <Route path="/teams" element={<Teams />} />
+      </Routes>
+
     </div>
   );
 }
