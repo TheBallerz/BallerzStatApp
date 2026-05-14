b9644c172b4957fc20789ac709ef9bece190787a
kennedysuon <ksuon@calpoly.edu>
2026-05-13 21:53:35 -0700

    Trigger frontend deployment

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -14,6 +14,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/layout/Layout';
 import Fantasy from '../pages/Fantasy/Fantasy';
 
+
 function App() {
   return (
     <Routes>
